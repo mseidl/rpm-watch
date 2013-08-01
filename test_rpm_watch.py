572bcd5453a7c2d0bb7b668844ad44d143d98b87
Martin Seidl <mseidl@suse.de>
2013-08-01 22:21:55 +0200

    fixed test cases

--- test_rpm_watch.py
+++ test_rpm_watch.py
@@ -4,16 +4,31 @@
 
 import unittest
 import sys
+import os
+import mock
+from StringIO import StringIO
 from rpm_watch import *
 
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 class TestRPMWatch(unittest.TestCase):
     '''Test cases for RPMWatch'''
     
     def setUp(self):
         self.rpmw = RPMWatch()
-        self.set1 = [1, 2, 3]
-        self.set2 = [2, 3, 4]
+        self.set1 = {1:'a', 2:'b', 3:'c'}
+        self.set2 = {2:'b', 3:'c', 4:'d'}
+        self.rpm1 = {'name1': [('1','1.1'),0],
+                     'name2': [('2.8.2','2.2'),0],
+                     'name3': [('1.1.10', '4.99'),0],
+                     'name4': [('8.88-git1', '1.0'),0]
+                     }
+        self.rpm2 = {'name1': [('1','1.10'),0], 
+                     'name2': [('2.9.2','2.2'),0],
+                     'name3': [('1.1.10', '4.99'),0],
+                     'name4': [('7.88-git', '1.0'),0]
+                     }
+                        
 
     def tearDown(self):
         pass
@@ -23,15 +38,26 @@ class TestRPMWatch(unittest.TestCase):
         self.assertTrue(self.rpmw.known_rpms)
         self.assertTrue(self.rpmw.installed_rpms)
         self.assertEqual(self.rpmw.known_rpms, self.rpmw.installed_rpms)
+    
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_print_diff_installed(self, mock_stdout):
+        print_diff(self.set1, self.set2, 'Installed')
+        self.assertEquals(mock_stdout.getvalue(), 'Installed: 1\n')
 
-    def test_print_diff(self):
-        print_diff(self.set1, self.set2)
-        output = sys.stdout.getvalue().strip()
-        self.assertEquals(output, 'Difference: 1')
-        sys.stdout.flush()
-        print_diff(self.set2, self.set1, 'Foo')
-        output2 = sys.stdout.getvalue().strip()
-        self.assertEquals(output2, 'Foo: 4')
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_print_diff_removed(self, mock_stdout):
+        print_diff(self.set2, self.set1, 'Removed')
+        self.assertEquals(mock_stdout.getvalue(), 'Removed: 4\n')
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_updated_rpms(self, mock_stdout):
+        updated_rpms(self.rpm2, self.rpm1)
+        results = mock_stdout.getvalue().split('\n')
+        print results
+        self.assertEquals(results[0], 'package name4 downgraded')
+        self.assertEquals(results[1], 'package name2 upgraded')
+        self.assertEquals(results[2], 'package name1 upgraded')
+
 
 if __name__ == '__main__':
     assert not hasattr(sys.stdout, "getvalue")
